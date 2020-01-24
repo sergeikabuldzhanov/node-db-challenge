b7c69ad1dcf8269a6278f26ec0ca65d054d118ac
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-24 15:42:25 +0500

    Validation middleware for projects

---
+++ server/validationMiddleware.js
@@ -0,0 +1,32 @@
+const projectDb = require("../data/helpers/project-model");
+const checkProjectIdValid = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const project = await projectDb.getById(id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      res.status(404).json({ message: `No project with matching id` });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const validateProjectData = (req, res, next) => {
+  if (req.body) {
+    const { name, description, completed } = req.body;
+    if (name) {
+      req.changes = { name, description, completed: completed ? true : false };
+      next();
+    } else {
+      res.status(400).json({ message: `Missing required fields` });
+    }
+  } else {
+    res.status(400).json({ message: `Missing request body` });
+  }
+};
+module.exports = {
+  checkProjectIdValid,
+  validateProjectData
+};
